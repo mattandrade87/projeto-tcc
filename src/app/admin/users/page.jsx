990c5b6f5c05997e969a1766c9681b283c909284
mattandrade87 <mateus.vitor.andrade@gmail.com>
2025-11-14 13:35:33 -0300

    refactor

--- src/app/admin/users/page.jsx
+++ src/app/admin/users/page.jsx
@@ -5,6 +5,7 @@ import Loader from "@/components/ui/Loader";
 import { useToast } from "@/context/ToastContext.jsx";
 import Link from "next/link";
 import { Title } from "@/app/main-page/components";
+import { BackButton } from "@/components/ui";
 
 export default function UsersListPage() {
   const [items, setItems] = useState([]);
@@ -27,6 +28,7 @@ export default function UsersListPage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center px-4 py-8">
       <div className="bg-white rounded-2xl shadow-2xl p-8 w-full max-w-5xl">
+        <BackButton />
         <div className="mb-4 text-center">
           <Title>Usuários</Title>
         </div>
